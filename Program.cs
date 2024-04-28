2dc380d3f903f30226963904e2c500b101110f6d
Marcus Aurelio Oliveira Campos <marcuswebdesigner@yahoo.com.br>
2024-04-27 23:54:44 -0300

    Desafio e testes concluidos

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Teste Nokia:");
+Smartphone nokia = new Nokia(numero: "987654321", modelo: "N1", imei: "1234567890", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Waze");
+
+Console.WriteLine("---------------");
+
+Console.WriteLine("Teste Iphone");
+Smartphone iphone = new Iphone(numero: "123456789", modelo: "i15 Pro", imei: "9876543210", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Ifood");
